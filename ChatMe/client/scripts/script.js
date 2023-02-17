ef201c3d402d09c27f7646d1b9fa6967784bbcff
Andrea Campostrini <118365481+AndreaCampostrini@users.noreply.github.com>
2023-02-17 00:37:13 +0000

    openpgp

--- ChatMe/client/scripts/script.js
+++ ChatMe/client/scripts/script.js
@@ -1,50 +1,25 @@
+//openpgp non viene importato
 var socket = io();
-
-
 (async () => {
-  const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
-      type: 'ecc', 
-      curve: 'curve25519',
-      userIDs: [{ name: document.getElementById("nickname").value, email: document.getElementById("email").value }], 
-      passphrase: document.getElementById("password").value, 
-      format: 'armored' 
+  const { privateKey, publicKey } = await openpgp.generateKey({
+    type: "rsa", // Type of the key
+    rsaBits: 4096, // RSA key size (defaults to 4096 bits)
+    userIDs: [
+      {
+        name: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+      },
+    ], // you can pass multiple user IDs
+    passphrase: document.getElementById("password").value, // protects the private key
   });
-
   console.log(privateKey);
-  console.log(publicKey);      
-  console.log(revocationCertificate);
+  console.log(publicKey);
 })();
 
-
 function register() {
-  let nickname = document.getElementById("nickname").value;
+  let username = document.getElementById("username").value;
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  
-  socket.emit("register", nickname, email, password);
+  console.log("ssa");
+  socket.emit("register", username, email, password, publicKey);
 }
-socket.on("register", (nome) => {
-  let paragrafo = document.getElementById("paragrafo");
-  paragrafo.innerText += " " + nome;
-});
-
-socket.on("confirm", (msg) => {
-  let registration = document.getElementById("register");
-  registration.innerText = msg;
-});
-
-socket.on("newuser", (msg) => {
-  let users = document.getElementById("users");
-  users.innerText = msg;
-});
-
-socket.on("nicknames", (nicknames) => {
-  let lista = document.getElementById("nicknames");
-  lista.innerHTML = "";
-  for (let i = 0; i < nicknames.length; i++) {
-    let nickname = nicknames[i];
-    let item = document.createElement("li");
-    item.innerHTML = nickname;
-    lista.appendChild(item);
-  }
-});
--- ChatMe/server/app.js
+++ ChatMe/server/app.js
@@ -2,7 +2,66 @@ const express = require("express");
 const config = require("./config.js");
 const socketio = require("socket.io");
 const openpgp = require("openpgp");
-const sqlite3 = require('sqlite3').verbose();
+var AES = require("crypto-js/aes");
+const sqlite3 = require("sqlite3").verbose();
+
+const passphrase = `sgk#^Ca!5s=Gq;jY|~tm98@Wyoze3nJAl-80Ecc%1vx33uG7qd]+nbLRi4-]oK![hbu@B>}l\qkFQFyKw[d|eXC#\`nBaZ|qg.Wk"KW8joLbVHx6Vim#LJ[2x7?O@mP{nFBYk\BRKzIW94vfWynq\oiE:h|9<:1l*gtT3o8l=^h%iL-JYB?Y?3$1p+e>2rsD:_E-@>nS:*s6|E(as\`2.Oo&4w[1QZLV!JG8|RwfX)k|W)'t}Kh$/4S!<[5dv*C}i=<-&0*t4Qp_NY9h[Qn\}aW=fk@LZL=o,+K_8]2v3e<h~;#LH'~ln\`F^Cc\}GC$ly'?*.E!JyWy8mFv2gI4Y+dWCLf=n=(2qsZX/'rWR.~$c5\lz?<Qy81ZZ=HkF{7?O.wQ$M2"9Jr;wlYhcwP"qkPCy06>{_r0btfNdN9*di>oa,so{f$8\`;la*&L-5%@?5!\CT6vli9.oO&pr/+:9!D[1dG-k'WQ6O9*A\~Y%j2DNt2d;"sKZz&IpE7qkRCH"US:`; // what the private key is encrypted with
+
+function encryptData(data) {
+  (async () => {
+    //generazione chiavi
+    const { privateKey, publicKey, revocationCertificate } =
+      await openpgp.generateKey({
+        type: "ecc",
+        curve: "curve25519",
+        userIDs: [{ name: "Jon Smith", email: "jon@example.com" }],
+        passphrase: `sgk#^Ca!5s=Gq;jY|~tm98@Wyoze3nJAl-80Ecc%1vx33uG7qd]+nbLRi4-]oK![hbu@B>}l\qkFQFyKw[d|eXC#\`nBaZ|qg.Wk"KW8joLbVHx6Vim#LJ[2x7?O@mP{nFBYk\BRKzIW94vfWynq\oiE:h|9<:1l*gtT3o8l=^h%iL-JYB?Y?3$1p+e>2rsD:_E-@>nS:*s6|E(as\`2.Oo&4w[1QZLV!JG8|RwfX)k|W)'t}Kh$/4S!<[5dv*C}i=<-&0*t4Qp_NY9h[Qn\}aW=fk@LZL=o,+K_8]2v3e<h~;#LH'~ln\`F^Cc\}GC$ly'?*.E!JyWy8mFv2gI4Y+dWCLf=n=(2qsZX/'rWR.~$c5\lz?<Qy81ZZ=HkF{7?O.wQ$M2"9Jr;wlYhcwP"qkPCy06>{_r0btfNdN9*di>oa,so{f$8\`;la*&L-5%@?5!\CT6vli9.oO&pr/+:9!D[1dG-k'WQ6O9*A\~Y%j2DNt2d;"sKZz&IpE7qkRCH"US:`, // protects the private key
+        format: "armored",
+      });
+    console.log(privateKey);
+    console.log(publicKey);
+    console.log(revocationCertificate);
+
+    //lettura chiavi
+    const public = await openpgp.readKey({
+      armoredKey: publicKey,
+    });
+
+    const private = await openpgp.decryptKey({
+      privateKey: await openpgp.readPrivateKey({
+        armoredKey: privateKey,
+      }),
+      passphrase,
+    });
+
+    //cifratura messaggio
+    const encrypted = await openpgp.encrypt({
+      message: await openpgp.createMessage({ text: data }),
+      encryptionKeys: public
+    });
+    console.log(encrypted);
+  })();
+}
+encryptData("chiave da criptare");
+/*(async () => {
+
+  const message = await openpgp.readMessage({
+    armoredMessage: encrypted, // parse armored message
+  });
+  const { data: decrypted, signatures } = await openpgp.decrypt({
+    message,
+    verificationKeys: publicKey, // optional
+    decryptionKeys: privateKey,
+  });
+  console.log(decrypted); // 'Hello, World!'
+  // check signature validity (signed messages only)
+  try {
+    await signatures[0].verified; // throws on invalid signature
+    console.log("Signature is valid");
+  } catch (e) {
+    throw new Error("Signature could not be verified: " + e.message);
+  }
+})();*/
 
 const app = express();
 app.use(express.static("../client"));
@@ -12,80 +71,76 @@ const users = [
     email: "user1@example.com",
     username: "user1",
     password: "password1",
-    eta: 20
-  }
+    eta: 20,
+  },
 ];
 
 var db;
-db = new sqlite3.Database('./db/users.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err && err.code == "SQLITE_CANTOPEN") {
-        createDatabase();
-        return;
-        } else if (err) {
-            console.log("Getting error " + err);
-            exit(1);
-    }
-    insertUser(db, users[0]);
+db = new sqlite3.Database("./db/users.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err && err.code == "SQLITE_CANTOPEN") {
+    createDatabase();
+    return;
+  } else if (err) {
+    console.log("Getting error " + err);
+    exit(1);
+  }
+  insertUser(db, users[0]);
 });
 
 function createDatabase() {
-  var newdb = new sqlite3.Database('./db/users.db', (err) => {
-      if (err) {
-          console.log("Getting error " + err);
-          exit(1);
-      }
-      createTables(newdb);
+  var newdb = new sqlite3.Database("./db/users.db", (err) => {
+    if (err) {
+      console.log("Getting error " + err);
+      exit(1);
+    }
+    createTables(newdb);
   });
 }
 
 function createTables(newdb) {
-  newdb.exec(`
+  newdb.exec(
+    `
   create table users (
       username text primary key not null,
       email text not null,
       password text not null,
       eta int 
-  );`, (err) => {
-    console.log("Error creating tables: " + err);
-  });
+  );`,
+    (err) => {
+      console.log("Error creating tables: " + err);
+    }
+  );
 }
 
 function getUsers(db) {
-  db.all(`
-  select * from users`, (err, rows) => {
-      rows.forEach(row => {
-          console.log(row);
+  db.all(
+    `
+  select * from users`,
+    (err, rows) => {
+      rows.forEach((row) => {
+        console.log(row);
       });
-  });
+    }
+  );
 }
 
 function insertUser(db, newUser) {
   console.log(newUser);
-  db.all(`insert into users (username, email, password, eta)
-  values (?, '?', '?', '?');`, [newUser.username, newUser.email, newUser.password, newUser.eta], (err, rows) => {
+  db.all(
+    `insert into users (username, email, password, eta)
+  values (?, '?', '?', '?');`,
+    [newUser.username, newUser.email, newUser.password, newUser.eta],
+    (err, rows) => {
       if (err) {
-          console.log("Error inserting user: " + err);
+        console.log("Error inserting user: " + err);
       }
-  });
+    }
+  );
 }
 
 const server = app.listen(config.port, () => {
   console.log("Server in ascolto sulla porta " + config.port);
 });
-
-const publicKey = 'xjMEY+OA6hYJKwYBBAHaRw8BAQdAE2OJacHeP3Is1YPbia1aaWRKO3o9qQ1mTd8JpQiJ0F3NG1BpYXJkaSA8cGlhcmRpQGV4YW1wbGUuY29tPsKMBBAWCgA+BQJj44DqBAsJBwgJEAIDKbBMPQe/AxUICgQWAAIBAhkBAhsDAh4BFiEEVYABPTDZBtd6GDW1AgMpsEw9B78AAL2zAP9mMnKN+zeLmY6btip8EBttEoW07LFdvKQtPod6Bg1rRwEA/m5J29C5tA4d877+k8c5oU+MFaL7/rG4LGymPZEGFAbOOARj44DqEgorBgEEAZdVAQUBAQdArxxEvRZtPAYTB+J3sJFsQ9Ds2LpYWLfVFuGguzJgq2MDAQgHwngEGBYIACoFAmPjgOoJEAIDKbBMPQe/AhsMFiEEVYABPTDZBtd6GDW1AgMpsEw9B78AAAGsAQDcLYVpeMmgWue/mAHVzWZa9SYuIVXfiYNalGO79il0RwEA7UR489bRj/7ksSnM0lQOkhuxJVJihThnmTi7kNEjnw0==99ZC';
-const privateKey = 'xYYEY+OA6hYJKwYBBAHaRw8BAQdAE2OJacHeP3Is1YPbia1aaWRKO3o9qQ1mTd8JpQiJ0F3+CQMILXavJP7Dgv/g+0TSGXPuCL5sGl1eArmEr5Ydg5MNMU9naYBswVesaGQIUUo6MwiWQ8ib88+2pth0ZQ9EedKxwky/qdd82MxQqlpKcWEbLc0bUGlhcmRpIDxwaWFyZGlAZXhhbXBsZS5jb20+wowEEBYKAD4FAmPjgOoECwkHCAkQAgMpsEw9B78DFQgKBBYAAgECGQECGwMCHgEWIQRVgAE9MNkG13oYNbUCAymwTD0HvwAAvbMA/2Yyco37N4uZjpu2KnwQG20ShbTssV28pC0+h3oGDWtHAQD+bknb0Lm0Dh3zvv6TxzmhT4wVovv+sbgsbKY9kQYUBseLBGPjgOoSCisGAQQBl1UBBQEBB0CvHES9Fm08BhMH4newkWxD0OzYulhYt9UW4aC7MmCrYwMBCAf+CQMIDk90TM9eDcbgh91DAFhL/oS94DiGXAaAZgiEYSCxKQ6nkWHrg0m5EUGBARcv7ecF+5q77aFnEe2ggrphzPWNd7fQmxNzDPMknYK5ZW6FmcJ4BBgWCAAqBQJj44DqCRACAymwTD0HvwIbDBYhBFWAAT0w2QbXehg1tQIDKbBMPQe/AAABrAEA3C2FaXjJoFrnv5gB1c1mWvUmLiFV34mDWpRju/YpdEcBAO1EePPW0Y/+5LEpzNJUDpIbsSVSYoU4Z5k4u5DRI58N=mJgw';
-
-
-
-
-async function decrypt(cryptedmessage) {
-  await privateKey.decrypt(passphrase);
-  const decrypted = await openpgp.decrypt({
-    message: cryptedmessage,
-    privateKeys: [privateKey],
-  });
-};
 const io = socketio(server);
 // var nicknames = [];
 // io.on("connection", (socket) => {
@@ -99,7 +154,7 @@ const io = socketio(server);
 //     );
 //     socket.broadcast.emit("newuser", socket.nome + " si è unito alla chat.");
 //     io.emit("newuser", socket.nome);
-      
+
 //   });
 // });
 
@@ -107,16 +162,19 @@ io.on("connection", (socket) => {
   socket.on("getPublicKey", (publicKeyClient) => {
     io.emit("publicKey", publicKey);
   });
-  socket.on("register",(cemail,cpassword,cnickname) => {
+  socket.on("register", (cemail, cpassword, cnickname) => {
     email = decrypt(cemail);
     password = decrypt(cpassword);
     nickname = decrypt(cnickname);
     const newUser = {
       email: email,
       username: nickname,
-      password: password
-    };    
+      password: password,
+    };
     users.push(newUser);
-    io.emit("confirm", "Registrazione avvenuta con successo. Benvenuto " + cnickname);
+    io.emit(
+      "confirm",
+      "Registrazione avvenuta con successo. Benvenuto " + cnickname
+    );
   });
 });
