ec625f4a909e76e819836a7c1bc93572f442a697
Andrea Campostrini <118365481+AndreaCampostrini@users.noreply.github.com>
2023-02-18 16:50:33 +0000

    prove import

--- ChatMe/client/scripts/script.js
+++ ChatMe/client/scripts/script.js
@@ -1,44 +1,54 @@
 /*Socket.io*/
 var socket = io();
-
+//var CryptoJS = require("crypto-js");
+//var openpgp = require("openpgp");
 function register() {
-  socket.emit("getPublicKey");
-  console.log("emit getPublicKey")
+    socket.emit("getPublicKey");
 }
-socket.on("publicKey", (publicKey) => {
-  // Genera una chiave di 256 bit in formato word array
-  var key = CryptoJS.lib.WordArray.random(32);
 
-  // Converte la chiave in formato base64
-  var keyBase64 = CryptoJS.enc.Base64.stringify(key);
-  let crypted_nickname = encryptPGP(encryptAES(document.getElementById("username").value, keyBase64), publicKey);
-  let crypted_email = encryptPGP(encryptAES(document.getElementById("email").value, keyBase64), publicKey);
-  let crypted_password = encryptPGP(encryptAES(document.getElementById("password").value, keyBase64), publicKey);
-  let crypted_key = encryptPGP(keyBase64, publicKey);
-  console.log(crypted_key)
-  //socket.emit("register", crypted_email, crypted_password, crypted_nickname, crypted_key);
-  console.log("emit register")
+socket.on("publicKey", (publicKey) => {
+    // Genera una chiave di 256 bit in formato word array
+    var key = CryptoJS.lib.WordArray.random(32);
+
+    // Converte la chiave in formato base64
+    var keyBase64 = CryptoJS.enc.Base64.stringify(key);
+
+    let crypted_nickname = encryptPGP(
+        encryptAES(document.getElementById("username").value, keyBase64),
+        publicKey
+    );
+
+    let crypted_email = encryptPGP(
+        encryptAES(document.getElementById("email").value, keyBase64),
+        publicKey
+    );
+    let crypted_password = encryptPGP(
+        encryptAES(document.getElementById("password").value, keyBase64),
+        publicKey
+    );
+    let crypted_key = encryptPGP(keyBase64, publicKey);
+    socket.emit("register", crypted_email, crypted_password, crypted_nickname, crypted_key);
 });
 
 /*OpenPGP*/
 function encryptPGP(data, publicKey) {
-  (async () => {
-    //lettura chiavi
-    const key = await openpgp.readKey({
-      armoredKey: publicKey
-    });
+    (async () => {
+        //lettura chiavi
+        const key = await openpgp.readKey({
+            armoredKey: publicKey,
+        });
 
-    //cifratura messaggio
-    const encrypted = await openpgp.encrypt({
-      message: await openpgp.createMessage({ text: data }),
-      encryptionKeys: key
-    });
+        //cifratura messaggio
+        const encrypted = await openpgp.encrypt({
+            message: await openpgp.createMessage({ text: data }),
+            encryptionKeys: key,
+        });
 
-    return encrypted;
-  })();
+        return encrypted;
+    })();
 }
 
 /*CryptoJS*/
 function encryptAES(data, key) {
-  return CryptoJS.AES.encrypt(data, key).toString();
+    return CryptoJS.AES.encrypt(data, key).toString();
 }
